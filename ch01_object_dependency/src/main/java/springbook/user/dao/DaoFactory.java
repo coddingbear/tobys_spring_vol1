88b6fb3e788047a78d54d61733e3cab799b1d9c5
CodingBear <surfwon@naver.com>
2018-09-15 09:19:33 +0900

    1.4 제어의 역전(Ioc) - 오브젝트 팩토리 활용 - 생성 오브젝트 코드 수정

--- ch01_object_dependency/src/main/java/springbook/user/dao/DaoFactory.java
+++ ch01_object_dependency/src/main/java/springbook/user/dao/DaoFactory.java
@@ -2,20 +2,24 @@ package springbook.user.dao;
 
 /**
  * 제어의 역전(IoC) - 오브젝트 팩토리
- * 1-16 DAO 생성 메소드의 추가로 인해 발생하는 중복
+ * 1-17 DAO 생성 오브젝트 코드 수정
  */
 public class DaoFactory {
 	public UserDao userDao() {
-		return new UserDao(new DConnectionMaker());
+		return new UserDao(connectionMaker());
 	}
 	
-	/* ConnectionMaker 구현 클래스를 선정하고 생성하는 코드의 중복
+	/* 
 	public AccountDao accountDao() { 
-		return new AccountDao(new DConnectionMaker());
+		return new AccountDao(connectionMaker());
 	}
 	
 	public MessageDao messageDao() {
-		return new MessageDao(new DConnectionMaker());
+		return new MessageDao(connectionMaker());
 	}
 	*/
+	
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();  // 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
+	}
 }
