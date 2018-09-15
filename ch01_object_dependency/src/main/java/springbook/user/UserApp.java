8e6e7fe166c3469492a97f1d225f545a8c1c0214
CodingBear <surfwon@naver.com>
2018-09-15 09:08:12 +0900

    1.4 제어의 역전(IoC) - 오브젝트 팩토리

--- ch01_object_dependency/src/main/java/springbook/user/UserApp.java
+++ ch01_object_dependency/src/main/java/springbook/user/UserApp.java
@@ -1,25 +1,20 @@
 package springbook.user;
 
 import java.sql.SQLException;
-
-import springbook.user.dao.ConnectionMaker;
-import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
 /**
  * UserDao 클라이언트 메인 테스트 
  * @author CodingBear
+ * 
+ * 1-15 팩토리를 사용하도록 수정한 UserApp
  */
 public class UserApp {
-	// 1-12 관계설정 책임이 추가된 UserDao 클라이언트인 main() 메소드
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// UserDao가 사용할 ConnectionMaaker 구현 클래스를 결정하고 오브젝트를 만든다.
-		ConnectionMaker connectionMaker = new DConnectionMaker();  
 		
-		UserDao dao = new UserDao(connectionMaker); 
-		// 1. UserDao 생성
-		// 2. 사용할 ConnectionMaker 타입의 오브젝트 제공. -> 결국 두 오브첵트 사이의 의존관계 설정 효과
+		UserDao dao = new DaoFactory().userDao();
 		
 		User user = new User();
 		user.setId("whiteship");
