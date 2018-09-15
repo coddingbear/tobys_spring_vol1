3c6f40930cb39706c4dc11b5a542d1773ce44b1d
CodingBear <surfwon@naver.com>
2018-09-15 09:14:57 +0900

    1.4 제어의 역전(IoC) - 오브젝트 팩토리 활용 - 생성메소드 추가로 중복 코드 발생

--- ch01_object_dependency/src/main/java/springbook/user/dao/DaoFactory.java
+++ ch01_object_dependency/src/main/java/springbook/user/dao/DaoFactory.java
@@ -2,13 +2,20 @@ package springbook.user.dao;
 
 /**
  * 제어의 역전(IoC) - 오브젝트 팩토리
- * 1-14 UserDao의 생성 책임을 맡은 팩토리 클래스
+ * 1-16 DAO 생성 메소드의 추가로 인해 발생하는 중복
  */
 public class DaoFactory {
 	public UserDao userDao() {
-		// 팩토리의 메소드는 UserDao 타입의 오브젝트를 어떻게 만들고 어떻게 준비시킬지를 결정한다.
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
+		return new UserDao(new DConnectionMaker());
 	}
+	
+	/* ConnectionMaker 구현 클래스를 선정하고 생성하는 코드의 중복
+	public AccountDao accountDao() { 
+		return new AccountDao(new DConnectionMaker());
+	}
+	
+	public MessageDao messageDao() {
+		return new MessageDao(new DConnectionMaker());
+	}
+	*/
 }
