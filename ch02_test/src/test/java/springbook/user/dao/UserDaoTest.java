8d9d9408ce17983dc9fc8be4ca2876d4a8b8f866
CodingBear <surfwon@naver.com>
2018-09-20 23:42:32 +0900

    2.4 스프링 테스트 적용 - DI와 테스트

--- ch02_test/src/test/java/springbook/user/dao/UserDaoTest.java
+++ ch02_test/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -3,12 +3,16 @@ package springbook.user.dao;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.dao.UserDao;
@@ -19,24 +23,20 @@ import springbook.user.domain.User;
  */
 @RunWith(SpringJUnit4ClassRunner.class) // 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
 @ContextConfiguration(locations="/applicationContext.xml") // 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
+@DirtiesContext // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 테스트 컨텍스트 프레임워크에 알려준다.
 public class UserDaoTest {
-	
+	// 2-19 UserDao를 직접 DI 받도록 만든 테스트
 	@Autowired
-	private ApplicationContext context; // 테스트 오브젝트가 만들어지고 나면 스프링 테스트 컨텍스트에 의해 자동으로 값이 주입된다.
-	
 	private UserDao dao;
+	
 	private User user1;
 	private User user2;
 	private User user3;
 	
 	@Before // JUnit이 제공하는 애노테이션 @Test 메소드가 실행되기 전에 먼저 실행되야 하는 메소드를 정의한다.
-	public void setUp() {
-		// 2-18 확인용 코드 추가
-		System.out.println(this.context);
-		System.out.println(this);
-		
-		this.dao = this.context.getBean("userDao", UserDao.class);
-		
+	public void setUp() {	
+		DataSource dataSource = new SingleConnectionDataSource( // 테스트에서 UserDao가 사용할 DataSource 오브젝트를 직접생성한다.
+				"jdbc:mysql://localhost/testdb", "spring", "book", true);
 		this.user1 = new User("gyumee", "박성철", "springno1");
 		this.user2 = new User("leegw700", "이길원", "springno2");
 		this.user3 = new User("bumjin", "박범진", "springno3");
