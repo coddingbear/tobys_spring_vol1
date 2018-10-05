8019499968465fbdf81e3122c6fe68c587d6ed22
CodingBear <surfwon@naver.com>
2018-10-06 00:11:04 +0900

    3.1 예외처기 기능을 갖춘 DAO

---
+++ ch03_template/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,110 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import springbook.user.domain.User;
+
+/**
+ * JDBC를 이용한 등록과 조회 기능이 있는 UserDao 클래스
+ * 3.1.1 예외처리 기능을 갖춘 DAO
+ */
+public class UserDao {
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	// 사용자 데이터 추가
+	public void add(User user) throws SQLException {
+		
+		Connection c = dataSource.getConnection();
+		
+		//2. SQL을 담은 Statement 또는 PreparedStatement 를 만든다.
+		PreparedStatement ps = c.prepareStatement(
+				"INSERT INTO users(id, name, password) VALUES(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2,  user.getName());
+		ps.setString(3, user.getPassword());
+		
+		// 3. 만들어진 Statement를 실행한다.
+		ps.executeUpdate();
+		
+		// 4. 작업중에 생성된 Connection, Statement, ResultSet 리소스를 닫아 준다.
+		ps.close();
+		c.close();
+	}
+	
+	// 사용자 데이터 가져오기
+	public User get(String id) throws SQLException {
+
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement(
+				"SELECT * FROM users WHERE id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		User user = null;  // User는 null 상태로 초기화해놓는다.
+		if(rs.next()) {    // id를 조건으로 한 쿼리의 결과가 있으면 User 오브젝트를 만들고 값을 넣어준다.
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
+		
+		rs.close();
+		ps.close();
+		c.close();
+		// 결과가 없으면 User는 null 상태 그대로일 것이다. 이를 확인해서 예외를 던져준다.
+		if(user == null) throw new EmptyResultDataAccessException(1);
+		return user;
+	}
+	
+	// 3-1 예외 발생 시에도 리소스를 반환하도록 수정
+	public void deleteAll() throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		
+		try { // 예외가 발생할 가능성이 있는 코드를 모두 try 블록으로 묶어준다.
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("DELETE FROM users");
+			ps.executeUpdate(); 
+		} catch (SQLException e) { // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다
+			throw e;  // 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다
+		} finally { // finally이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
+			if(ps != null) try { ps.close(); } catch(SQLException e) {}
+			if(c != null)  try { c.close();  } catch(SQLException e) {}
+		}
+	}
+	
+	// 3-3 JDBC 예외 처리를 적용한 getCount() 메소드
+	public int getCount() throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("SELECT COUNT(*) FROM users");
+			
+			rs = ps.executeQuery(); // ResultSet도 다양한 SQLException이 발생할 수 있는 코드이므로 try 블록안에 둬야 한다.
+			rs.next();
+			return rs.getInt(1);
+			
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if(rs != null) try { rs.close(); } catch(SQLException e) {}
+			if(ps != null) try { ps.close(); } catch(SQLException e) {}
+			if(c != null)  try { c.close();  } catch(SQLException e) {}
+		}
+	}
+}
---
+++ ch03_template/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,102 @@
+package springbook.user.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+/**
+ * 2-17 스프링 텍스트 컨텍스트를 적용한 UserDaoTest
+ */
+@RunWith(SpringJUnit4ClassRunner.class) // 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+@ContextConfiguration(locations="/applicationContext.xml") // 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
+@DirtiesContext // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 테스트 컨텍스트 프레임워크에 알려준다.
+public class UserDaoTest {
+	// 2-19 UserDao를 직접 DI 받도록 만든 테스트
+	@Autowired
+	private UserDao dao;
+	
+	private User user1;
+	private User user2;
+	private User user3;
+	
+	@Before // JUnit이 제공하는 애노테이션 @Test 메소드가 실행되기 전에 먼저 실행되야 하는 메소드를 정의한다.
+	public void setUp() {	
+		DataSource dataSource = new SingleConnectionDataSource( // 테스트에서 UserDao가 사용할 DataSource 오브젝트를 직접생성한다.
+				"jdbc:mysql://localhost/testdb", "spring", "book", true);
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
+		
+	} // 각 테스트 메소드에 반복적으로 나타났던 코드를 제거하고 별도의 메소드로 옮긴다.
+
+	@Test
+	public void addAndGet() throws SQLException { 		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		// 첫 번째 User의 id로 get()을 실행하면 첫 번째 User의 값을 가진 오브젝트를 돌려주는지 확인한다.
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		
+		// 두 번째 User에 대해서도 같은 방법으로 검증한다.
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+	}
+	
+	// 2-11 getCount() 테스트
+	@Test
+	public void count() throws SQLException {	
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+		
+	}
+	
+	// 2-13 get() 메소드의 예외상황에 대한 테스트
+	@Test(expected=EmptyResultDataAccessException.class) // 테스트 중에 발생할 것으로 기대하는 예외 클래스를 지정해 준다.
+	public void getUserFailure() throws Exception {
+	
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unkown_id"); // 이 메소드 실행 중에 예외가 발생해야 한다. 예외가 발생하지 않으면 테스트가 실패한다.
+	}
+	
+/* JUnit이 하나의 테스트 클래스를 가져와 테스트를 수행하는 방식은 다음과 같다.
+  1. 테스트 클래스에서 @Test가 붙은 public이고 void형이며 파라미터가 없는 테스트 메소드를 모드 찾는다.
+  2. 테스트 클래스의 오브젝트를 하나 만든다.
+  3. @Before가 붙은 메소드가 있으면 실행한다.
+  4. @Test가 붙은 메소드를 하나 호출하고 테스트 결과를 저장해둔다.
+  5. @After가 붙은 메소드가 있으면 실행한다.
+  6. 나머지 테스트 메소드에 대해 2~5번을 반복한다.
+  7. 모든 테스트의 결과를 종합해서 돌려준다.
+*/	
+}
